a522169bce5dc8c653fb602b2ed6b594f1c2d0fa
Yuksalbek Rakhimjonov <yuksal97@gmail.com>
2020-03-27 12:52:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -33,7 +33,8 @@ end
 def apply_coupons(cart, coupons)
   counter = 0 
   while counter < coupons.length
-  card_item
+  card_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+  couponed_item_name = "#{coupons[counter"
 end
 
 def apply_clearance(cart)
